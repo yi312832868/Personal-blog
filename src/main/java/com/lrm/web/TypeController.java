ef6bb00d7d3204b67d839b8a479d3857aae0626e
yi312832868 <ypfei666@gmail.com>
2019-12-19 10:27:50 -0500

    typeInput

--- src/main/java/com/lrm/web/TypeController.java
+++ src/main/java/com/lrm/web/TypeController.java
@@ -1,6 +1,7 @@
 package com.lrm.web;
 
 
+import com.lrm.po.Type;
 import com.lrm.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -20,10 +23,26 @@ public class TypeController {
     private TypeService typeService;
 
     @GetMapping("/types")
-    public String types(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC)
+    public String types(@PageableDefault(size = 3, sort = {"id"}, direction = Sort.Direction.DESC)
                                 Pageable pageable,
                         Model model){
         model.addAttribute("page",typeService.listType(pageable));
         return "admin/types";
+    }
+
+    @GetMapping("/types/input")
+    public String input(){
+        return "admin/types-input";
+    }
+
+    @PostMapping("/types")
+    public String post(Type type, RedirectAttributes attributes){
+        Type t = typeService.saveType(type);
+        if(t == null){
+            attributes.addFlashAttribute("message","operation failed");
+        } else {
+            attributes.addFlashAttribute("message","operation success");
+        }
+        return "redirect:/admin/types";
     }
 }
